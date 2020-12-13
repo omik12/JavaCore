0b29766ed0386511090577682b032f3c5da31f9d
omik12 <akbarhossinqa@gmail.com>
2020-12-13 09:05:44 -0500

    first commit

---
+++ src/com/nonePrimitiveDataType/JavaStringAndVariable.java
@@ -0,0 +1,13 @@
+package com.nonePrimitiveDataType;
+
+public class JavaStringAndVariable {
+    public static void main(String[] args) {
+        String Test = "This is String";
+        String Test2 = "This is string 2";
+        String Test3 = "This is string 3";
+        String Test4 = "This is string 4";
+        System.out.println(Test4 + Test3 + Test2 + Test);
+
+    }
+}
+
---
+++ src/com/primitiveDataType/PrintHelloWorld.java
@@ -0,0 +1,13 @@
+package com.primitiveDataType;
+
+public class PrintHelloWorld {
+    public static void main(String[]args){
+        System.out.println("Hello World");
+        System.out.println("My Name Is Omi");
+        System.out.println("My Number Is 917 517 9095");
+
+
+    }
+
+
+}
